491682c8bea4ab9ba20e4f9840e04ed8b8f43685
DK <d.dinesh1011@gmail.com>
2019-09-07 02:16:46 +0800

    tests

--- golang/src/code_fights/chess_knight.go
+++ golang/src/code_fights/chess_knight.go
@@ -9,28 +9,44 @@ func chessKnight(cell string) int {
 
 	counter := 0
 
-	if (x-2) > 0 {
-		if (y+1) < 9 {counter++}
-		if (y - 1) > 0 {counter++}
+	if (x - 2) > 0 {
+		if (y + 1) < 9 {
+			counter++
+		}
+		if (y - 1) > 0 {
+			counter++
+		}
 	}
-	if (x+2) < 9 {
-		if (y+1) < 9 {counter++}
-		if (y - 1) > 0 {counter++}
+	if (x + 2) < 9 {
+		if (y + 1) < 9 {
+			counter++
+		}
+		if (y - 1) > 0 {
+			counter++
+		}
 	}
-	if (y-2) > 0 {
-		if (x+1) < 9 {counter++}
-		if (x - 1) > 0 {counter++}
+	if (y - 2) > 0 {
+		if (x + 1) < 9 {
+			counter++
+		}
+		if (x - 1) > 0 {
+			counter++
+		}
 	}
-	if (y+2) < 9 {
-		if (x+1) < 9 {counter++}
-		if (x - 1) > 0 {counter++}
+	if (y + 2) < 9 {
+		if (x + 1) < 9 {
+			counter++
+		}
+		if (x - 1) > 0 {
+			counter++
+		}
 	}
 
 	return counter
 }
 
-
-func main(){
+func main() {
 
 	fmt.Println(chessKnight("g1"))
+
 }
